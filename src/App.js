c94d3ef51dbbf6bb5f3cff84fe0b59eea610e65c
James <jamescscalise@gmail.com>
2020-09-26 13:09:22 -0500

    connect navbar and router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome to the news aggregator. Here you can find the top stories from major news sources.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        
+        <NavBar />
+      </div>
+    </Router>
   );
 }
 
