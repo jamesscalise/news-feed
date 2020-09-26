06f91acce6759c5a83a597a632c27e221d788223
James <jamescscalise@gmail.com>
2020-09-26 13:00:24 -0500

    complete NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,39 @@ import { NavLink } from 'react-router-dom';
 
 class NavBar extends Component {
 
+    
+
     render (){
-        return(
-            <div></div>
-        )
+        return (
+            <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
+                <NavLink 
+                style={{ marginRight: '10px' }} 
+                to="/"
+              > 
+                Home
+              </NavLink>
+              <NavLink 
+              style={{ marginRight: '10px' }} 
+              to="/"
+              >The New York Times</NavLink>
+              <NavLink 
+              style={{ marginRight: '10px' }} 
+              to="/"
+              >BBC News</NavLink>
+              <NavLink 
+              style={{ marginRight: '10px' }} 
+              to="/index"
+              >NPR</NavLink>
+              <NavLink 
+              style={{ marginRight: '10px' }} 
+              to="/index"
+              >The Washington Post</NavLink>
+              <NavLink 
+              style={{ marginRight: '10px' }} 
+              to="/index"
+              >Reuters</NavLink>
+            </div>
+          );
     }
 }  
 
