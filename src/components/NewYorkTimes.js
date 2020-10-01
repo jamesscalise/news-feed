321f9eab15e70cd6d8ea123615bda6be6354399c
James <jamescscalise@gmail.com>
2020-09-30 19:05:24 -0500

    finish nyt page

--- src/components/NewYorkTimes.js
+++ src/components/NewYorkTimes.js
@@ -15,8 +15,10 @@ class NewYorkTimes extends Component {
         var xml = new XMLParser().parseFromString(data);
         let filtered = xml.children[0].children.filter(d => d.name ==="item")
         console.log(filtered)
+        let newFiltered = filtered.map(item => item.children)
+        console.log(newFiltered)
         this.setState({
-            items: filtered
+            items: newFiltered
         })
     })
 	.catch(function(error) {
@@ -28,9 +30,24 @@ class NewYorkTimes extends Component {
         items: []
     }
 
+    renderItems = () => {
+        return(
+        this.state.items.map(item => (
+            <div className = "item">
+                <a class='btn btn-link' href={item[1].value}><h3>{item[0].value}</h3></a>
+                <p>{item[4].value}</p>
+            </div>
+
+        ))
+        )
+    }
+
     render (){
         return(
-            <div>test</div>
+            <div className="feed">test
+                {this.renderItems()}
+            </div>
+            
         )
     }
 }  
