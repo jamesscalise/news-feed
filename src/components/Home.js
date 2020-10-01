ff275f58aa89d275b04be97c9970e0888d56fa89
James <jamescscalise@gmail.com>
2020-09-30 19:32:19 -0500

    add date

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 
 class Home extends Component {
 
+    renderTime = () => {
+        var today = new Date();
+
+        const monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"];
+
+        var date = (monthNames[today.getMonth()])+' '+today.getDate()+', '+today.getFullYear();
+        return date
+
+    }
+
     render (){
         return(
-            <div>Welcome!</div>
+            <div>
+                <p>Welcome! Today's date is {this.renderTime()}</p>
+            </div>
         )
     }
 }  
