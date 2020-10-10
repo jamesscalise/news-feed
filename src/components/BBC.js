00c38d3c5b46603cf752b47e64001c4b42d25b60
James <jamescscalise@gmail.com>
2020-10-10 15:48:30 -0500

    create react components for files

---
+++ src/components/BBC.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class BBC extends Component {
+
+    render (){
+        return(
+            <div></div>
+        )
+    }
+}  
+
+export default BBC
---
+++ src/components/NPR.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NPR extends Component {
+
+    render (){
+        return(
+            <div></div>
+        )
+    }
+}  
+
+export default NPR
